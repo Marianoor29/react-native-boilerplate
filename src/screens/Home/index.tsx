0fdbbd798d1bb879fd847b743bd1ab31a859db6b
Maria Noor <76403209+Marianoor29@users.noreply.github.com>
2024-04-18 16:02:54 +0100

    home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -15,7 +15,7 @@ const Home = ({ navigation }: any) => {
       <StatusBar barStyle="dark-content" backgroundColor={AppColors.white} />
       <SafeAreaView style={styles.SafeAreaView}>
         <View style={styles.container}>
-          <CustomText text={'Wellcome to Home'}/>
+          <CustomText text={'Welcome Home'}/>
           <Button
             onPress={() => navigation.navigate('Profile')}
             text={'Go To Profile'}
